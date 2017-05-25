738981c612a52fc229423aec3d437b98a0a14b96
Iain Woodburn <enjkw13@gmail.com>
2017-05-24 23:31:12 -0600

    Create MyMalloc.c
    Added the description comment.

---
+++ MyMalloc.c
@@ -0,0 +1,12 @@
+/*
+ * CS252: MyMalloc Project
+ *
+ * The current implementation gets memory from the OS
+ * every time memory is requested and never frees memory.
+ *
+ * You will implement the allocator as indicated in the handout,
+ * as well as the deallocator.
+ * 
+ * You will also need to add the necessary locking mechanisms to 
+ * support multi-threaded programs.
+ */
